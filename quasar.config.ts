5e4dd3bbacadc84a4e8dd78c9405a14a775f0d90
SnB <skullandbonesdao@gmail.com>
2025-03-16 16:22:04 +0100

    Set publicPath for Quasar build configuration
    Uncommented and set the publicPath to '/app.foodshelf.quasar/' in the Quasar configuration file. This ensures the application is served from the correct base URL during deployment.

--- quasar.config.ts
+++ quasar.config.ts
@@ -33,7 +33,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
-      //publicPath: '/app.foodshelf.quasar/',
+      publicPath: '/app.foodshelf.quasar/',
       target: {
         browser: ['chrome87'],
         node: 'es2020',
